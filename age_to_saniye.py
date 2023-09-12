1cd8ec3c4b2f2ba3d3fc35b7d32f3e32ad32dc06
Fatemeh Maleki Almani <143957405+FatemehMalekiAlmani@users.noreply.github.com>
2023-09-12 22:37:23 +0330

    Add files via upload

---
+++ age_to_saniye.py
@@ -0,0 +1,9 @@
+from os import system
+system('cls')
+ss = 3.156e7
+age = int(input("Enter age:"))
+second = age * ss 
+print("Second = ", second)
+
+#برای نوشتن 3.156*10^7 باید بصورت خط سوم عمل کنیم
+#ینی بنویسم 3.126e7
