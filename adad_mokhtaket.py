d551d00bfbbe55e5d1c35f29c70765d8c9b866fb
Fatemeh Maleki Almani <143957405+FatemehMalekiAlmani@users.noreply.github.com>
2023-09-23 17:57:29 +0330

    Add files via upload

---
+++ adad_mokhtaket.py
@@ -0,0 +1,8 @@
+from os import system
+system('cls')
+a=int(input('please enter real number: '))
+b=int(input('please enter image number: '))
+z=complex(a,b)
+print('your complex number is: ', z)
+
+# برای ساختن ععد مخلوط از دستور کامپلکس استفاده میکنند
